1ff3593cadc00e7bb56b22cf202bd6ac94a43fa3
hjjung <hjjung@enliple.com>
2021-05-13 18:52:12 +0900

    글 등록, 추천 작업중

---
+++ src/main/java/com/semobook/board/domain/Board.java
@@ -0,0 +1,24 @@
+package com.semobook.board.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@Entity
+public class Board {
+    @Id
+    private long id;
+    private String name;
+    private String contents;
+
+    @Builder
+    public Board(long id, String name, String contents){
+        this.id = id;
+        this.name = name;
+        this.contents = contents;
+    }
+
+}
