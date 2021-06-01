503b72826612f2c58843367c479c651d54bd2eeb
hyejinzz <hyejinj810@gmail.com>
2021-06-01 23:27:42 +0900

    SEMO-53 순환참조를 막기위한 BookReview 추가설정

---
+++ src/main/java/com/semobook/bookReview/dto/BookReviewDto.java
@@ -0,0 +1,30 @@
+package com.semobook.bookReview.dto;
+
+import com.semobook.book.domain.Book;
+import com.semobook.bookReview.domain.BookReview;
+import com.semobook.user.domain.UserInfo;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class BookReviewDto {
+    private int rating;
+    private String reviewContents;
+    private LocalDateTime createDate;
+    private int declaration;
+    private String isbn;
+    private String userName;
+//    private UserInfo userInfo;
+
+    public BookReviewDto(BookReview bookReview) {
+        rating = bookReview.getRating();
+        reviewContents = bookReview.getReviewContents();
+        createDate = bookReview.getCreateDate();
+        declaration = bookReview.getDeclaration();
+        isbn = bookReview.getBook().getIsbn();
+        userName = bookReview.getUserInfo().getUserName();
+//        book = bookReview.getBook();
+//        userInfo = bookReview.getUserInfo();
+    }
+}
