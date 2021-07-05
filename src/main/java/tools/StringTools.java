52f0d4dbf5908fc5fe10d3c9b1322b485a658ed5
hyejinzz <hyejinj810@gmail.com>
2021-07-06 06:23:45 +0900

    SEMO-104 Update : add parameger to stringConvToList

--- src/main/java/tools/StringTools.java
+++ src/main/java/tools/StringTools.java
@@ -1,7 +1,5 @@
 package tools;
 
-import com.semobook.common.SemoConstant;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -26,14 +24,14 @@ public class StringTools {
 
 
     /**
-     * String을 | 구본자 사용해서 List로 변환
+     * String을 | 구분자 사용해서 List로 변환
      *
      * @param basicString List로 변환할 String
+     * @param separator 구분자
      * @author hyejinzz
      * @since 2021-07-04
      **/
-    public static List<String> StringConvToListUsingBackSlash(String basicString) {
-        StringBuilder sb = new StringBuilder();
-        return Arrays.asList(basicString.split(SemoConstant.BACKSLASH_VARTICAL_BAR));
+    public static List<String> stringConvToList(String basicString, String separator) {
+        return Arrays.asList(basicString.split(separator));
     }
 }
