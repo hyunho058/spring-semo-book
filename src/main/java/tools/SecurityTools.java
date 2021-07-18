2536a60394483c80eabeb685eaeb9c102127a626
hyunho058 <hyunho058@naver.com>
2021-07-18 15:25:37 +0900

    Update change password encryption

--- src/main/java/tools/SecurityTools.java
+++ src/main/java/tools/SecurityTools.java
@@ -1,14 +1,33 @@
 package tools;
 
-import javax.xml.bind.DatatypeConverter;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class SecurityTools {
-    public static String md5(String input) throws NoSuchAlgorithmException {
-        MessageDigest md = MessageDigest.getInstance("MD5");
-        md.update(input.getBytes());
-        byte[] digest = md.digest();
-        return DatatypeConverter.printHexBinary(digest).toUpperCase();
+
+//    public static String md5(String input) throws NoSuchAlgorithmException {
+//        MessageDigest md = MessageDigest.getInstance("MD5");
+//        md.update(input.getBytes());
+//        byte[] digest = md.digest();
+//        return DatatypeConverter.printHexBinary(digest).toUpperCase();
+//    }
+
+    public static String md5(String pwd) {
+        String MD5 = "";
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(pwd.getBytes());
+            byte byteData[] = md.digest();
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < byteData.length; i++) {
+                sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            MD5 = sb.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            MD5 = null;
+        }
+        return MD5;
     }
 }
