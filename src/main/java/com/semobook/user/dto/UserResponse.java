a0353ccb0c5e5e774788bafec5700d639af8c955
hyunho058 <hyunho058@naver.com>
2021-07-18 19:42:56 +0900

    Change code value name in UserResponse

--- src/main/java/com/semobook/user/dto/UserResponse.java
+++ src/main/java/com/semobook/user/dto/UserResponse.java
@@ -11,16 +11,16 @@ import lombok.NoArgsConstructor;
 public class UserResponse {
 
     @Schema(description = "성공 코드" , example = "성공 : hd1004")
-    StatusEnum code;
+    StatusEnum hCode;
     @Schema(description = "메시지")
-    String message;
+    String hMessage;
     @Schema(description = "데이터")
     Object data;
 
     @Builder
-    public UserResponse(StatusEnum code, String message, Object data) {
-        this.code = code;
-        this.message = message;
+    public UserResponse(StatusEnum hCode, String hMessage, Object data) {
+        this.hCode = hCode;
+        this.hMessage = hMessage;
         this.data = data;
     }
 }
