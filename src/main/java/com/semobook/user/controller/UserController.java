8e19c32aa45ca5521f65f81508713149190e9b32
hyunho058 <hyunho058@naver.com>
2021-10-18 14:46:25 +0900

    SEMO-164 REFACTOR: change uri in UserInfoController

--- src/main/java/com/semobook/user/controller/UserController.java
+++ src/main/java/com/semobook/user/controller/UserController.java
@@ -13,24 +13,45 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @Tag(name = "UserController")
-@RequestMapping("/user")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
     //모든 회원 조회
     @Operation(description = "모든 회원조회")
-    @GetMapping(value = "/all{page}")
-    public ResponseEntity<UserResponse> getUserAll(@Parameter @PathVariable int page) {
+    @GetMapping(value = "/users{page}")
+    public ResponseEntity<UserResponse> getUserAllCon(@Parameter @PathVariable int page) {
         return ResponseEntity.ok(userService.findAllUser(page));
     }
 
 
     //id로 회원조회
     @Operation(description = "회원조회")
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<UserResponse> getUserByUserId(@Parameter @PathVariable String id) {
+    @GetMapping(value = "/user/{id}")
+    public ResponseEntity<UserResponse> getUserByUserIdCon(@Parameter @PathVariable String id) {
         return ResponseEntity.ok(userService.findByUserId(id));
+    }
+
+    //회원가입
+    @Operation(description = "회원가입")
+    @PostMapping("/user/new")
+    public ResponseEntity<UserResponse> signUpCon(@Parameter @RequestBody UserSignUpRequest userSignUpRequest) {
+        return ResponseEntity.ok(userService.signUp(userSignUpRequest));
+    }
+
+    //회원탈퇴
+    @Operation(description = "회원탈퇴")
+    @DeleteMapping("/user")
+    public ResponseEntity<UserResponse> deleteUserCon(@Parameter @RequestBody UserDeleteRequest userDeleteRequest) {
+        return ResponseEntity.ok(userService.deleteUser(userDeleteRequest));
+    }
+
+    //회원정보 수정
+    @Operation(description = "회원정보수정")
+    @PutMapping("/user")
+    public ResponseEntity<UserResponse> updateUserCon(@Parameter @RequestBody UserChangeUserInfoRequest updateUser) {
+        return ResponseEntity.ok(userService.updateUser(updateUser));
     }
 
     //로그인
@@ -40,35 +61,14 @@ public class UserController {
         return ResponseEntity.ok(userService.signIn(userSignInRequest));
     }
 
-    //회원가입
-    @Operation(description = "회원가입")
-    @PostMapping("/signup")
-    public ResponseEntity<UserResponse> signUpCon(@Parameter @RequestBody UserSignUpRequest userSignUpRequest) {
-        return ResponseEntity.ok(userService.signUp(userSignUpRequest));
-    }
-
-    //회원탈퇴
-    @Operation(description = "회원탈퇴")
-    @PostMapping("/delete")
-    public ResponseEntity<UserResponse> deleteUserCon(@Parameter @RequestBody UserDeleteRequest userDeleteRequest) {
-        return ResponseEntity.ok(userService.deleteUser(userDeleteRequest));
-    }
-
-    //회원정보 수정
-    @Operation(description = "회원정보수정")
-    @PostMapping("/update")
-    public ResponseEntity<UserResponse> updateUserCon(@Parameter @RequestBody UserChangeUserInfoRequest updateUser) {
-        return ResponseEntity.ok(userService.updateUser(updateUser));
-    }
-
     @Operation(description = "회원 정보")
-    @PostMapping("/userInfo")
-    public ResponseEntity<UserResponse> userInfoWithReviewCountCon(@Parameter @RequestBody UserInfoRequest userInfoRequest){
-        return ResponseEntity.ok(userService.userInfoWithReviewCount(userInfoRequest));
+    @GetMapping("/user/info/{userNo}")
+    public ResponseEntity<UserResponse> userInfoWithReviewCountCon(@Parameter @PathVariable long userNo){
+        return ResponseEntity.ok(userService.userInfoWithReviewCount(userNo));
     }
 
     @Operation(description = "send email")
-    @PostMapping("/send-email")
+    @PostMapping("/user/help/email")
     public ResponseEntity<UserResponse> sendEmailCon(@Parameter @RequestBody MailRequest mailRequest){
         return ResponseEntity.ok(userService.mailSend(mailRequest));
     }
@@ -80,14 +80,9 @@ public class UserController {
     }
 
     @Operation(description = "find pw")
-    @PostMapping("/find-pw")
-    public ResponseEntity<UserResponse> findId(@Parameter @RequestBody FindPwRequest find){
-
-        return ResponseEntity.ok(userService.findPw(find.getUserId()));
+    @GetMapping("/user/help/pwInquiry")
+    public ResponseEntity<UserResponse> findIdCon(@Parameter @RequestParam(name = "userId") String userId){
+        return ResponseEntity.ok(userService.findPw(userId));
     }
-
-
-
-
 
 }
